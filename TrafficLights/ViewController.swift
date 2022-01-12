85ef5173a6a68b74c556f2aa2564c857c605c5d6
Ula <1295593@mail.ru>
2022-01-12 14:00:11 +0800

    HW.2

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -26,9 +26,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.layer.cornerRadius = 10
-        redView.layer.cornerRadius = redView.frame.width/2
-        yellowView.layer.cornerRadius = yellowView.frame.size.width/2
-        greenView.layer.cornerRadius = greenView.frame.size.width/2
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
         redView.alpha = 0.3
         yellowView.alpha = 0.3
         greenView.alpha = 0.3
